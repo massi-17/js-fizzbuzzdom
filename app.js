ec2169fbea1f38a8ede87851b4ec2f079f1a7431
massi-17 <massimianisilvia@gmail.com>
2022-02-07 18:25:48 +0100

    celle fizzbuzz metodo 2

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // container nel DOM
 const container = document.getElementById("container");
+let cella = document.createElement('div');
 
 // ciclo numeri da 1 a 100
  for (let i = 1; i <= 100; i++) { 
     // inizio dalla condizione più specifica alla più comune
     if (i % 3 == 0 && i % 5 == 0){ // numeri multipli di 3(diviso 3(%3) danno(==)0) E(&&) 5(diviso 5(%5) danno(==)0)
         console.log("fizzbuzz"); 
-        // aggiunta DIV con class box fizzbuzz
-        container.innerHTML += '<div class="box fizzbuzz">fizzbuzz</div>';
+        // aggiunta DIV, attributi e classi con il JS
+        container.appendChild(cella);
+        cella.className = 'box';  
+        cella.style.backgroundColor = '#F0466F';
+        cella.textContent = 'fizzbuzz';
     } else if (i % 5 == 0) { // numeri multipli di 5     
         console.log("buzz");
         // aggiunta DIV con class box buzz
@@ -23,4 +27,5 @@ const container = document.getElementById("container");
 
 }
 
+//con le celle fizzbuzz ho creato i div e le classi non inserendo direttamente l'html ma settandole col js per provare
 
